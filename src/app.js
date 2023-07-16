3b7378c39f4d268271de6f4d9c677ee0eeb9d74a
yujiichikawa <thiagogelinski20@gmail.com>
2023-07-16 17:31:25 -0300

    Atualizando email
    Criando metodo put para atualizar um atributo do user

--- src/app.js
+++ src/app.js
@@ -11,12 +11,24 @@ function verificarUsuarioExistente(request,response,next){
   const existente = users.some(user=>user.cpf === cpf)
 
   if(existente){
-    return response.status(400).json("Usuario já existente").send()
+    return response.status(400).json("Usuario existente").send()
   }
-
+  
   return next()
 }
 
+function buscarUsuario(request,response,next){
+  const {cpf} = request.headers
+
+  const us = users.find(user=>user.cpf === cpf)
+
+  if(!us){
+    return response.status(400).json("Usuario não existente").send()
+  }
+  
+  request.user = us
+  return next()
+}
 server.get('/',(request,response)=>{
   return response.json({msg:`Numero de usuarios cadastrados: ${users.length}`})
 })
@@ -40,6 +52,15 @@ server.get('/list-users',(request,response)=>{
   if(users.length === 0) return response.status(200).json("Nenhum usuário cadastrado").send()
 
   return response.status(200).json(users).send()
+})
+
+server.put("/update-user",buscarUsuario,(request,response)=>{
+  const {email} = request.body
+  const {user} = request
+  
+  user.email = email
+  
+  return response.status(200).json("E-mail Atualizado").send()
 })
 
 server.listen(3333,()=>{

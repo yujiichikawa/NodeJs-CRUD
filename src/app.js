d50b848f9d96aab5dd5307f1223999bf143a2bb5
yujiichikawa <thiagogelinski20@gmail.com>
2023-07-16 16:52:02 -0300

    Cadastro de usuários
    criando método post e funçâo para verificar usuário existente

--- src/app.js
+++ src/app.js
@@ -3,9 +3,40 @@ const server = express()
 
 server.use(express.json())
 
+var users = []
+
+function verificarUsuarioExistente(request,response,next){
+  const {cpf} = request.headers
+
+  const existente = users.some(user=>user.cpf === cpf)
+
+  if(existente){
+    return response.status(400).json("Usuario já existente").send()
+  }
+
+  return next()
+}
+
 server.get('/',(request,response)=>{
-  return response.json({msg:'Home'})
+  return response.json({msg:`Numero de usuarios cadastrados: ${users.length}`})
 })
+
+server.post('/create-user',verificarUsuarioExistente,(request,response)=>{
+  const { cpf, cnh, cidadeNasc, estadoNasc, email, telefone, renavam} = request.headers
+
+  users.push({
+    cpf,
+    cnh,
+    cidadeNasc,
+    estadoNasc,
+    email,
+    telefone,
+    renavam
+  })
+  return response.status(201).json("Usuario cadastrado").send()
+})
+
+
 server.listen(3333,()=>{
   console.log('### Servidor ativo')
 })
